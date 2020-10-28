3df001443bb927ee42fc582f73e4c1a5fb68c971
User-Hrsat <15937712794@163.com>
2020-10-28 16:05:08 +0800

    调整

--- Features.py
+++ Features.py
@@ -8,98 +8,118 @@ import re
 import time
 from urllib import request
 
-import jieba  # 有点差劲唉,可能是没用好
-
+#import jieba  # 有点差劲唉,可能是没用好
 #jieba.set_dictionary('./BadLanguage/dict.txt')
 #jieba.load_userdict('./BadLanguage/badlanguage.txt')
 
-def Analysis(timestamp, membernames, memberid, messages):
-                                                    #文本分析
-#    timestamp = datetime.datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S")
-#    date = {
-#            '时间' : timestamp,
-#            '昵称' : membernames,
-#            'ID' : memberid,
-#            '消息' : messages
-#            }
-#
-#    jsoncode = json.dumps(date)
-#    jsonfile = open('History.json', 'a+')
-#    jsonfile.write(jsoncode+'\n')
-#    jsonfile.close()                                #施工中...
+class Feathures():
+ 
+    def __init__(self, timestamp: str, membernames: str, memberid: int, messages: str):
+                                                                                #清洗文本,去除换行、特殊符号以及去重
 
-#    cutwords = jieba.lcut(messages)
-#    words = open('./BadLanguage/badlanguage.txt', 'r')
-#    for w in cutwords:
-#        if w+'\n' in words.readlines():
-#            words.close()
-#            return '不要讲脏话'
-    if messages == '检测':
-        return '屑', 'text'
+        self.timestamp = timestamp
+        self.membernames = membernames
+        self.memberid = memberid
+        self.messages = messages
 
-def Clear(txt: str) -> list:                                     #清洗文本,去除换行、特殊符号以及去重
+        self.comms: list
+        spew = ['|', '&', '%']
+        switch = {
+            ':网抑云' : self.Cloudmusic,
+            ':image' : self.Image,
+            ':rss' : self.RSS,
+            ':zuan' : self.Zuan,
+            ':help' : self.Help,
+            'analysis': self.Analysis
+            }
+
+        if re.match('^:', self.messages) == None:                               #排除非特征信息,留做文本分析
+            self.comms.append('analysis')
+        else:
+            for i in spew:
+                self.comms = self.messages.replace(i, '\n')                     #去特殊字符
+            self.comms = self.comms.splitlines()                                #去换行
+            for i in self.comms:
+                if re.match('^:', i) != None:                                   #保留以:开头的字段
+                    if i == 'analysis':
+                        switch[i](timestamp, membernames, memberid, messages)
+                    elif re.match('^:ping', i):
+                        self.Ping(i)
+                    else:
+                        switch[i]()
+
+            #        self.comms.append(i)
+            #self.comms = list(set(self.comms))          #去重
+
+    def Analysis(self, timestamp, membernames, memberid, messages):
+                                                        #文本分析
+    #    timestamp = datetime.datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S")
+    #    date = {
+    #            '时间' : timestamp,
+    #            '昵称' : membernames,
+    #            'ID' : memberid,
+    #            '消息' : messages
+    #            }
+    #
+    #    jsoncode = json.dumps(date)
+    #    jsonfile = open('History.json', 'a+')
+    #    jsonfile.write(jsoncode+'\n')
+    #    jsonfile.close()                                #施工中...
     
-    spew = ['|', '&', '%']
-    comms: list
+    #    cutwords = jieba.lcut(messages)
+    #    words = open('./BadLanguage/badlanguage.txt', 'r')
+    #    for w in cutwords:
+    #        if w+'\n' in words.readlines():
+    #            words.close()
+    #            return '不要讲脏话'
+        if messages == '检测':
+            return '屑', 'text'
 
-    if re.match('^:', txt) == None:                 #排除非特殊信息,留做文本分析
-        comms.append('analysis')
-    else:
-        for i in spew:
-            txt = txt.replace(i, '\n')              #去特殊字符
-        txt = txt.splitlines()                      #去换行
-        for i in txt:
-            if re.match('^:', i) != None:           #保留以:开头的字段
-                comms.append(i)
-        comms = list(set(comms))                    #去重
-
-    return comms
-
-def Cloudmusic():
-
-    return '正在施工', 'text'
-
-def Noncomd(i):                                     #不存在的指令
-
-    if len(i) > 7:
-        return
-    else:
-        return f"没有{i}这条命令!", 'text'
-
-def Image():
-    num = random.randint(0, 2)
-    return f"resource/images/{num}.jpg", 'image'
-
-def Ping(i):
-    ip = re.match(":ping ((25[0-5])|(2[0-4]d)|(1dd)|([1-9]d)|d)(.((25[0-5])|(2[0-4]d)|(1dd)|([1-9]d)|d)){3}", i)
-    url = re.match(":ping [a-zA-Z0-9][-a-zA-Z0-9]{0,62}(.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+.?", i)
+    def Cloudmusic(self):
     
-    if ip != None or url != None:
-        resuatl = str()                             #预定义变量真不爽
-        i = list(i)
-        i.pop(0)                                    #去:号
-        i = ''.join(i)                              #转换为字符串
-        restr = os.popen(f"{i} -c 4")
-        for i in restr.readlines():
-            resuatl += i
-        return resuatl, 'text'
-
-    if ip == None and url == None:
-        return """正确用法:
+        return '正在施工', 'text'
+    
+    def Noncomd(self, i):                                     #不存在的指令
+    
+        if len(i) > 7:
+            return
+        else:
+            return f"没有{i}这条命令!", 'text'
+    
+    def Image(self):
+        num = random.randint(0, 2)
+        return f"resource/images/{num}.jpg", 'image'
+    
+    def Ping(self, i: str):
+        ip = re.match(":ping ((25[0-5])|(2[0-4]d)|(1dd)|([1-9]d)|d)(.((25[0-5])|(2[0-4]d)|(1dd)|([1-9]d)|d)){3}", i)
+        url = re.match(":ping [a-zA-Z0-9][-a-zA-Z0-9]{0,62}(.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+.?", i)
+        
+        if ip != None or url != None:
+            resuatl = str()                             #预定义变量真不爽
+            i = list(i)
+            i.pop(0)                                    #去:号
+            i = ''.join(i)                              #转换为字符串
+            restr = os.popen(f"{i} -c 4")
+            for i in restr.readlines():
+                resuatl += i
+            return resuatl, 'text'
+    
+        if ip == None and url == None:
+            return """正确用法:
 :ping IPor域名
 一定要填写正确的IP或域名哦!""", 'text'
-
-def RSS():
-    return '你说什么我听不懂', 'text'
-
-def Zuan():
-    response = request.urlopen("https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn")
-    zuan = response.read()
-    return zuan.decode('utf-8'), 'text'
-
-def Help():                                         #明明我这边排版好好的
-    return """用法: :[指令]
-
+    
+    def RSS(self):
+        return '你说什么我听不懂', 'text'
+    
+    def Zuan(self):
+        response = request.urlopen("https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn")
+        zuan = response.read()
+        return zuan.decode('utf-8'), 'text'
+    
+    def Help(self):                                         #明明我这边排版好好的
+        return """用法: :[指令]
+    
 :image      发送图片
 :ping       就是ping嘛
 :zuan       嘴臭一下
--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ from graia.application.entry import (At, Friend, GraiaMiraiApplication, Group,
                                      Session, Xml)
 from graia.broadcast import Broadcast
 
-import Features
+from Features import Feathures
 
 app = Broadcast(loop=asyncio.get_event_loop())
 
@@ -16,15 +16,14 @@ mirai = GraiaMiraiApplication(
         broadcast = app,
         connect_info = Session(
             host = "http://127.0.0.1:8080",
-            authKey = "",
-            account = 123,
+            authKey = "MeuPasswd",
+            account = 1291517893,
             websocket = True
         )
 )
 
 #authKey = ""
 #qq = 
-
 #mirai = Mirai(f"mirai://127.0.0.1:8080/?authKey={authKey}&qq={qq}", websocket=True)
 
 #@mirai.onStage("around")                           #脚本启动或关闭时给我发消息
@@ -41,63 +40,28 @@ async def event_gm(mirai: GraiaMiraiApplication, message: MessageChain, group: G
     membernames = member.memberName
     memberid = member.id                            #发送消息的人
 
-    command = Features.Clear(messages)              #清洗文本,提取指令
-
-#    Features.Analysis(timestamp, membernames, memberid, messages)
-                                                    #文本分析:刷屏、脏话、复读机
-
-#    global mesarr
-#    if len(mesarr) <= 3:
-#        mesarr.append(messages)
-#        print(mesarr)
-#    if len(mesarr) == 3:
-#        if mesarr[2] == mesarr[1]:
-#            await mirai.sendGroupMessage(
-#                    group.id,
-#                    [
-#                        Plain(text='匹配')
-#                        ]
-#                    )
-#        mesarr.pop(0)
-
     switch = {                                      #switch/case
             'text' : Plain,                         #消息组件复用
             'image' : Image.fromFileSystem,         #消息组件复用
-            'xml' : Xml,                     #消息组件复用
-            ':网抑云' : Features.Cloudmusic,
-            ':image' : Features.Image,
-            ':rss' : Features.RSS,
-            ':zuan' : Features.Zuan,
-            ':help' : Features.Help,
-            'analysis': Features.Analysis
+            'xml' : Xml,                            #消息组件复用
         }
 
-    async def sendmessage(i):
-
+    async def sendmessage(recall):
         try:
-            if i == 'analysis':
-                try:
-                    getmessages, infotype = switch[i](timestamp, membernames, memberid, messages)
-                except TypeError:
-                    return
-            else:
-                getmessages, infotype = switch[i]()
-        except KeyError:                            #需要参数或者匹配的
-            if re.match('^:ping', i):
-                getmessages, infotype = Features.Ping(i)
-            else:
-                return
+            remessage = recall[0]
+            infotype = recall[1]
+        except TypeError:
+            return
 
         await mirai.sendGroupMessage(
                 group.id,
                 [
                     At(target=memberid),
-                    switch[infotype](getmessages)
+                    switch[infotype](remessage)
                     ]
                 )
 
-    for i in command:                               #经过清洗提取的指令会逐一运行
-        await sendmessage(i)
+    await sendmessage(Feathures(timestamp, membernames, memberid, messages))
 
 if __name__ == "__main__":
     mirai.launch_blocking()
